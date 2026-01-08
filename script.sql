b9e7ddbe110a7e6d6cedeacac84aeefdf8a257fa
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-01-08 12:12:47 +0200

    Apprentissage du modèle MVC intro

---
+++ script.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE note_app;
+USE note_app;
+
+CREATE TABLE notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(100) NOT NULL,
+    contenu TEXT NOT NULL,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
