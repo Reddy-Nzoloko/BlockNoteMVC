3a25c5dba7124076fece02d84522633d9b87e6af
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-01-12 12:44:40 +0200

    l

--- script.sql
+++ script.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE note_app;
 USE note_app;
 
+-- premiere partie on doit enrichir le truc
 CREATE TABLE notes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titre VARCHAR(100) NOT NULL,
