9addd52b1ff689e00e6e6006d0d728747a707768
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-01-12 05:19:35 +0200

    Correction de l'erreur de la boucle

--- index.php
+++ index.php
@@ -4,27 +4,28 @@ require_once 'controllers/NoteController.php';
 
 $controller = new NoteController();
 
-// On regarde si une action est demandée dans l'URL (ex: index.php?action=ajouter)
-if (isset($_GET['action']) && $_GET['action'] == 'ajouter') {
-    $controller->sauvegarder();
-} else {
-    // Par défaut, on affiche la liste
-    $controller->afficherAccueil();
-}
-// ... (sous l'action ajouter)
-switch($_GET['action']) {
+// 1. On récupère l'action de l'URL, si elle n'existe pas, on utilise 'index' par défaut
+$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+// 2. Le "Router" : un seul switch pour diriger l'utilisateur au bon endroit
+switch($action) {
     case 'ajouter':
         $controller->sauvegarder();
         break;
+        
     case 'supprimer':
         $controller->supprimerNote();
         break;
+        
     case 'editer':
         $controller->editerNote();
         break;
+        
     case 'confirmer_modifier':
         $controller->mettreAJour();
         break;
+        
+    case 'index':
     default:
         $controller->afficherAccueil();
         break;
