ed0d3bdcf006776d84b2f81d55aaccc6023afc6b
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-01-13 12:23:05 +0200

    Mise à jour base de donnée

--- script.sql
+++ script.sql
@@ -8,3 +8,15 @@ CREATE TABLE notes (
     contenu TEXT NOT NULL,
     date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+-- 1. Création de la table des utilisateurs
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- 2. Ajout de la colonne user_id dans la table notes
+ALTER TABLE notes ADD COLUMN user_id INT;
+
+-- 3. (Optionnel) Créer un lien officiel entre les deux tables
+ALTER TABLE notes ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
